5c2946defe68b6505a40bef5deb158991df4670f
Devraj Babar <devadevrajdbabar149@gmail.com>
2023-02-22 13:01:13 +0530

    Payment Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:kickgo/pages/home_page.dart';
+import 'package:kickgo/pages/payment.dart';
 import 'package:kickgo/pages/register_page.dart';
 import 'package:kickgo/utils/routes.dart';
 import 'package:kickgo/widgets/themes.dart';
 import 'pages/login_page.dart';
+import 'package:pay/pay.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,11 +22,12 @@ class MyApp extends StatelessWidget {
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
       initialRoute: MyRoutes.loginRoute,
-      home: HomePage(),
+      home: PaymentPage(),
       routes: {
         MyRoutes.loginRoute: (context) => LoginPage(),
         MyRoutes.registerRoute: (context) => RegisterPage(),
         MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.paymentRoute: (context) => PaymentPage(),
       },
     );
   }
--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -2,4 +2,5 @@ class MyRoutes {
   static String loginRoute = "/login";
   static String registerRoute = "/register";
   static String homeRoute = "/home";
+  static String paymentRoute = "/payment";
 }
